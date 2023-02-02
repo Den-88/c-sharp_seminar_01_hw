7322ff12cd78e4709455638dcac087c84317ef3d
Denis Shakurov <den_shak@mail.ru>
2023-02-02 22:21:50 +0300

    add zadacha 23

---
+++ zadacha_23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+    PrintResult(n);
+}
+else
+{
+    System.Console.WriteLine("Число N меньше единицы!");
+}
+
+void PrintResult(int x)
+{
+    for (int i = 1; i <= x; i++)
+    {
+        System.Console.Write($"{Math.Pow(i, 3)} ");
+    }
+    System.Console.WriteLine();         //добавил чтобы bash был на следующей строке,так красивее 
+}
+
