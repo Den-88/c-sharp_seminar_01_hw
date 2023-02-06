3e83ba495ba9b326c2992b570241e2c12443eca6
Denis Shakurov <den_shak@mail.ru>
2023-02-07 00:44:02 +0300

    add zadacha 27

---
+++ zadacha_27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine(GetResult());
+
+string GetNumber()
+{
+    System.Console.Write("Введите число: ");
+    return Console.ReadLine();
+}
+
+string GetResult()
+{
+    string num_text = GetNumber();
+    int result = 0;
+    for (int i = 0; i < num_text.Length; i++)
+    {
+        if (int.TryParse(num_text[i].ToString(), out int num))
+        {
+            result += num;
+        }
+        else
+        {
+            return "Введено не число!";
+        }
+    }
+    return "Результат: " + result.ToString();
+}
