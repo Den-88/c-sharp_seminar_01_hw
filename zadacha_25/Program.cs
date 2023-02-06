d6ea02071efc8b7cf0c90465340c050675008d7e
Denis Shakurov <den_shak@mail.ru>
2023-02-07 00:02:27 +0300

    add zadacha 25

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+PrintResult();
+
+int GetNumber(char num_text)
+{
+    System.Console.Write($"Введите число {num_text}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetResult()
+{
+    int A = GetNumber('A');
+    int B = GetNumber('B');
+    int result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+void PrintResult()
+{
+    System.Console.WriteLine($"Результат: {GetResult()}");
+}
