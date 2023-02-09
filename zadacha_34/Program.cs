cc5416c1861dc3d78066cb5b9ab43596dc7f7821
Denis Shakurov <den_shak@mail.ru>
2023-02-10 00:18:45 +0300

    add zadacha 34

---
+++ zadacha_34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+System.Console.Write("Введите количество элементов массива: ");
+int[] arr = GenerateArray(Convert.ToInt32(Console.ReadLine()));
+PrintArray(arr);
+System.Console.WriteLine($"Количество чётных чисел в массиве: {GetCountEven(arr)}");
+
+int[] GenerateArray(int size)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+int GetCountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
