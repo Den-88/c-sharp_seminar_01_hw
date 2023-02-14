c943ad547bcad4aff651377fc2dce1f9245f22d5
Denis Shakurov <den_shak@mail.ru>
2023-02-14 13:12:57 +0300

    add zadacha 43

---
+++ zadacha_43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Координаты точки пересечения: (" + GetResult(ReadNum("b1"), ReadNum("k1"), ReadNum("b2"), ReadNum("k2")) + ")");
+
+double ReadNum(string str)
+{
+    System.Console.Write("Введите значение " + str + ": ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+string GetResult(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return $"{x}; {y}";
+}
